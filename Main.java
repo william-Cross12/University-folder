8559d957223dce2bf1bdcdcd419f813309f3c6a8
william cross <william.cross@stfc.ac.uk>
2023-02-03 13:27:41 +0000

    changed so says hi to the user using user input

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
+        sc.close();
     }
 }
