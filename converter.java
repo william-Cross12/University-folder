317fd34bff0381b437bfaca5876852ca0d379749
william cross <william.cross@stfc.ac.uk>
2023-02-03 14:37:04 +0000

    created a feet to miles converter task 8

---
+++ converter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class converter {
+    public static void main(String[] args)
+    {
+        int FEET_MILES = 5280;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of feet: ");
+        Double feet = sc.nextDouble();
+
+        Double miles = feet/FEET_MILES;
+
+        System.out.println("there are: " + miles + " miles in " + feet + " feet");
+
+        sc.close();
+    }
+}
