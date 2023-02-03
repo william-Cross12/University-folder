1e6d6c1c028e0b1e4f650622f736a284f6c7018b
william cross <william.cross@stfc.ac.uk>
2023-02-03 13:38:46 +0000

    created area Perimeter rectangle code task 3

---
+++ rectangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class rectangle {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rectangle Length: ");
+        int Length = sc.nextInt();
+        System.out.print("Enter  rectangle Height: ");
+        int Height = sc.nextInt();
+        int Area = Length * Height;
+        int Perimeter = ((Length*2) + (Height*2));
+        System.out.println("Area: " + Area);
+        System.out.println("Perimeter: " + Perimeter);
+
+        sc.close();
+    }
+}
