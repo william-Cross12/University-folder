b0ac53f477300a52a795d8d1d6ed4d0486751f61
william cross <william.cross@stfc.ac.uk>
2023-02-03 14:32:15 +0000

    made age.java print the number of days since you where born task 7

--- age.java
+++ age.java
@@ -1,17 +1,21 @@
-import java.time.Year;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
 public class age {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
+        System.out.print("Enter your Date of birth YYYY-MM-DD: ");
+        String age = sc.nextLine();
 
-        int year = Year.now().getValue();
+        LocalDate dob = LocalDate.parse(age); 
+        LocalDate now = LocalDate.now();
 
-        int year_born = (year - age);
+        long days = ChronoUnit.DAYS.between(dob, now);
 
-        System.out.println("you where born in: " + year_born);
+        System.out.println("days since birth: " + days);
+
+        
 
         sc.close();
     }
