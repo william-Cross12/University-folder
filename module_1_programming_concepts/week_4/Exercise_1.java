22278cff570cc9293aec27ded860886f743c2dd5
william cross <william.cross@stfc.ac.uk>
2023-02-24 13:18:36 +0000

    completed forst task

---
+++ module_1_programming_concepts/week_4/Exercise_1.java
@@ -0,0 +1,23 @@
+public class Exercise_1 {
+
+}
+
+class Module1
+{
+    private String code;
+    private String name;
+
+    public Module1(String Module_code, String Module_name)
+    {
+        this.code = Module_code;
+        this.name = Module_name;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getCode()
+    {
+        return this.code;
+    }
+}
